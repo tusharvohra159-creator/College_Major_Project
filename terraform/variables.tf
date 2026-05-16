d960703ee1c35ea5cf9bceb9de3847172028abeb
tushar26vohra <vohratushar23@gmail.com>
2026-05-16 11:48:57 +0530

    Final

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,28 +10,10 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for Public Subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for Private Subnet"
-  type        = string
-  default     = "10.0.2.0/24"
-}
-
 variable "instance_type" {
   description = "EC2 Instance type"
   type        = string
-  default     = "t2.micro"
-}
-
-variable "mongo_uri" {
-  description = "MongoDB Connection String"
-  type        = string
-  sensitive   = true
+  default     = "t3.micro"
 }
 
 variable "jwt_secret" {
